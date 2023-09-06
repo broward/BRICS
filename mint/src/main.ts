6b6e2a4b5f413a3066cbb22b5be9aeaa4f0e0be6
broward horne <bhorne@ensomata.com>
2023-09-05 23:50:53 -0700

    save

--- mint/src/main.ts
+++ mint/src/main.ts
@@ -1,19 +1,17 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { Realm } from './realm'
+import { realms } from './realm'
+import { Country } from './realm'
 import { Visitor } from './visitor'
 
 async function bootstrap() {
-  let BRICS: Realm[] = []
-  BRICS[0] = new Realm(1, "Brazil")
-  BRICS[1] = new Realm(2, 'Russia')
-  BRICS[2] = new Realm(3, 'India')
-  BRICS[3] = new Realm(4, 'China')
-  BRICS[4] = new Realm(5, 'South Africa')
 
-  let visitor = new Visitor(BRICS)
-  console.log(BRICS[0].realmName)
-
+ console.log(realms)
+  let visitor = new Visitor(realms)
+  var index = realms[0].country
+  console.log('my country is ' + realms[0].country)
+  console.log('my index is ' + Country[index])
 
   const app = await NestFactory.create(AppModule)
   await app.listen(3000)
--- mint/src/realm.ts
+++ mint/src/realm.ts
@@ -1,14 +1,29 @@
-export class Realm {
-    realmID: number;
-    realmName: string;
+export enum Country {
+  'BRAZIL',
+  'RUSSIA',
+  'INDIA',
+  'CHINA',
+  'SOUTH_AFRICA'
+}
 
-    constructor(id: number, name: string) {
-            this.realmName = name;
-            this.realmID = id;
+
+export class Realm {
+    country: Country
+
+    constructor(country: Country) {
+            this.country = country
     }
 
     getPool() : number {
-        console.log('it worked');
-        return 10000;
+        console.log('it worked for ' + this.country.valueOf());
+        return this.country.valueOf()
     }
 }
+
+export var realms: Realm[] = [
+    new Realm(Country.BRAZIL),
+    new Realm(Country.RUSSIA),
+    new Realm(Country.INDIA),
+    new Realm(Country.CHINA),
+    new Realm(Country.SOUTH_AFRICA),
+  ];
